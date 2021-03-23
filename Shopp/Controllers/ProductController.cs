4ae41f60c494c5f731d54a3a562f7b616690062a
Gustavo Corrêa <tcho32@gmail.com>
2021-03-23 10:35:25 -0300

    Finalizado a API

---
+++ Shopp/Controllers/ProductController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shopp.Models;
+using Shopp.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Shop.Controllers
+{
+    [Route("products")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        [AllowAnonymous]
+        public async Task<ActionResult<List<Product>>> Get([FromServices] DataContext context)
+        {
+            var products = await context.Products.Include(x => x.Category).AsNoTracking().ToListAsync();
+            return products;
+
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        [AllowAnonymous]
+        public async Task<ActionResult<Product>> GetById(int id, [FromServices] DataContext context)
+        {
+            var product = await context.Products.Include(x => x.Category).AsNoTracking().
+                FirstOrDefaultAsync(x => x.Id == id);
+            return product;
+        }
+
+        [HttpGet]
+        [Route("categories/{id:int}")]
+        [AllowAnonymous]
+        public async Task<ActionResult<List<Product>>> GetByCategory([FromServices] DataContext context, int id)
+        {
+            var products = await context.Products.Include(x => x.Category).
+                AsNoTracking().Where(x => x.CategoryId == id).ToListAsync();
+            return products;
+
+        }
+
+        [HttpPost]
+        [Route("")]
+        [Authorize(Roles = "employee")]
+        public async Task<ActionResult<List<Product>>> Post([FromBody] Product model,
+                                                     [FromServices] DataContext context)
+        {
+            try
+            {
+                //Verifica se os dados são validos
+                if (ModelState.IsValid)
+                {
+                    context.Products.Add(model);
+                    await context.SaveChangesAsync();
+                    return Ok(model);
+                }
+                else
+                {
+                    return BadRequest(ModelState);
+                }
+            }
+            catch
+            {
+                return BadRequest(new { message = "Não foi possível criar o Produto" });
+            }
+        }
+    }
+}
---
+++ Shopp/Startup.cs
@@ -0,0 +1,95 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Shopp.Data;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.ResponseCompression;
+using System.Linq;
+using Microsoft.OpenApi.Models;
+
+namespace Shopp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddResponseCompression(options =>
+            {
+                options.Providers.Add<GzipCompressionProvider>();
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/json" });
+            });
+            //Fazendo cache em toda a aplicação. exemplos de cahce em apenas um metodo em CategoryController
+            services.AddResponseCaching();
+            services.AddControllers();
+
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                };
+            });
+
+            services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
+            services.AddScoped<DataContext, DataContext>();
+
+            services.AddSwaggerGen(c =>
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Shopp Api", Version = "v1" }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shopp API v1");
+            });
+
+            app.UseRouting();
+
+            app.UseCors(x => x
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
